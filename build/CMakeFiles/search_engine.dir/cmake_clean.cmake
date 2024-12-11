101e67e4cf38935655b11f361695cf52ab95e239
root <root@LAPTOP-508DBURA.>
2024-12-11 20:25:56 +0000

    Initial commit

---
+++ build/CMakeFiles/search_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_engine.dir/src/main.cpp.o"
+  "CMakeFiles/search_engine.dir/src/main.cpp.o.d"
+  "search_engine"
+  "search_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

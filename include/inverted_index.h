30186d61e91b1455fc48d7c2c42f65c7f24f067c
root <root@LAPTOP-508DBURA.>
2024-12-15 16:23:03 +0000

    minor change

--- include/inverted_index.h
+++ include/inverted_index.h
@@ -9,18 +9,21 @@
 
 class InvertedIndex {
 public:
-    // We need a method to add a document to the index
+    // method to add a document to the index
     void addDocument(int docId, const std::string& content);
 
-    // We need a method to get all the documents that contain a term
+    // method to get all the documents that contain a given term
     std::vector<int> searchTerm(const std::string& term) const;
+
+    // method to process word
+    void processWord(int docId, const std::string& word);
 
 private:
     // The data structure used is a map
     // Each term is a key, and the values are list of document IDs that contain the term
     mutable std::unordered_map<std::string, std::vector<int>> index;
     mutable std::mutex index_mutex; // mutex is for thread safety
-    void processWord(int docId, const std::string& word);
+    
 };
 
 #endif
